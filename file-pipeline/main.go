f06d7c764f45a90a659a3f3da57d10f9e1850f37
victorejike <victorejike@gmail.com>
2026-03-31 21:55:43 +0100

    Title the first word but skip the word like  of

---
+++ file-pipeline/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Capitalize(s string) string {
+	skip := map[string]bool{
+		"a":   true,
+		"an":  true,
+		"the": true,
+		"and": true,
+		"but": true,
+		"or":  true,
+		"nor": true,
+		"on":  true,
+		"at":  true,
+		"to":  true,
+		"by":  true,
+		"in":  true,
+		"of":  true,
+		"up":  true,
+		"as":  true,
+		"is":  true,
+	}
+
+	words := strings.Fields(s)
+	for i, word := range words {
+		if _, ok := skip[word]; !ok || i == 0 {
+			words[i] = strings.Title(word)
+		}
+	}
+
+	return strings.Join(words, " ")
+
+}
+
+func main() {
+	s := "      upper   sentinel online  "
+
+	fmt.Println(Capitalize(s))
+}
